61bd60815ec81bcbf321b95a8dd8a0d89655858d
RamloA <36705833+RamloA@users.noreply.github.com>
2018-03-07 18:44:25 +0100

    New

--- app/src/main/java/com/example/pelvicfloortraining/HomeActivity.java
+++ app/src/main/java/com/example/pelvicfloortraining/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.pelvicfloortraining;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+    }
+
+    public void onDagbog(View view) {
+        Intent intent = new intent (this, dagbog.class);
+        startActivity(intent);
+
     }
 }
