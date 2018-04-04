d99d68af6a28e6938160bda501bc07d46cc7cafe
RamloA <36705833+RamloA@users.noreply.github.com>
2018-04-04 15:04:43 +0200

    4

--- app/src/main/java/com/example/pelvicfloortraining/Training.java
+++ app/src/main/java/com/example/pelvicfloortraining/Training.java
@@ -1,92 +1,48 @@
 package com.example.pelvicfloortraining;
 
-import android.bluetooth.BluetoothAdapter;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 
 
 public class Training extends AppCompatActivity {
-
-
-    private static final String TAG = "Training";
-    BluetoothAdapter mBluetoothAdapter;
-
-
-    private final BroadcastReceiver mBroadcastRecevier1 = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (action.equals(mBluetoothAdapter.ACTION_STATE_CHANGED)) {
-                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, mBluetoothAdapter.ERROR);
-
-                switch (state) {
-                    case BluetoothAdapter.STATE_OFF:
-                        Log.d(TAG, "anReceive: STATE OFF");
-                        break;
-                    case BluetoothAdapter.STATE_TURNING_OFF:
-                        Log.d(TAG, "mBroadcastReceiver1: STATE TURINING OOF");
-                        break;
-                    case BluetoothAdapter.STATE_ON:
-                        Log.d(TAG, "mBroadcastReceiver1: STATE ON");
-                        break;
-                    case BluetoothAdapter.STATE_TURNING_ON:
-                        Log.d(TAG, "mBroadcastReceiver1: TURNING ON");
-                        break;
-                }
-            }
-        }
-
-    };
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(mBroadcastRecevier1);
-    }
+    Button btnONOFF;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_training);
-        Button btnONOFF = (Button) findViewById(R.id.btnONOFF);
-
-
-
-        btnONOFF.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                enableDisablBT();
-            };
-        });
-
+         btnONOFF =  findViewById(R.id.btnONOFF);
 
     }
 
-    public void enableDisablBT() {
-
-        if (mBluetoothAdapter == null) {
-            Log.d(TAG, "enabledisableBT: Does not have BT caoabilities");
-        }
-        else if(!mBluetoothAdapter.isEnabled()){
-            Intent enableBTIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(enableBTIntent);
-            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-            registerReceiver(mBroadcastRecevier1, BTIntent);
-        }
-
-        else if(mBluetoothAdapter.isEnabled()){
-            mBluetoothAdapter.disable();
-
-            IntentFilter BTIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-            registerReceiver(mBroadcastRecevier1, BTIntent);
-        }
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
 }
+
+
+
+
+
+
+
